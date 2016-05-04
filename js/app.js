0f8dbb29a9bce2bf687860caa1049dc0a985a92b
bjahng <bjj34@yahoo.com>
2016-05-04 08:26:56 -0700

    Third draft

--- js/app.js
+++ js/app.js
@@ -1,11 +1,34 @@
 $(function() {
+
+	// Cache the Window object
+	var $window = $(window);
+	
+	// Parallax Backgrounds
+		
+	$('section[data-type="background"]').each(function(){
+		var $bgobj = $(this); // assigning the object
+		
+		$(window).scroll(function() {
+		
+			// Scroll the background at var speed
+			// the yPos is a negative value because we're scrolling it UP!								
+			var yPos = (-($window.scrollTop() / $bgobj.data('speed'))) + 75;
+			
+			// Put together our final background position
+			var coords = '50% '+ yPos + 'px';
+			
+			// Move the background
+			$bgobj.css({ backgroundPosition: coords });
+			
+		}); // end window scroll
+	});
 	
 	//set map options
 	var myLatLng = {lat: 37.578120, lng: -122.342642};
 	
 	var mapOptions = {
 		center: myLatLng,
-		zoom: 15,
+		zoom: 16,
 		mapTypeId: google.maps.MapTypeId.SATELLITE
 	};
 
